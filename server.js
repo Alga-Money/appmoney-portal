001223e480ccd2e5fbcb26a5a4be22a8e2561ebc
akuma <diogerafael@gmail.com>
2020-03-14 18:32:50 -0300

    Correcao diretorio do projeto

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
-app.use(express.static(__dirname + '/dist/NOME_DO_SEU_PROJETO'));
+app.use(express.static(__dirname + '/dist/appmoney-portal'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/NOME_DO_SEU_PROJETO/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/appmoney-portal/index.html'));
 });
 
 // Inicia a aplicação pela porta configurada
