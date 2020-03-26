48762e930741eea3a68d0554c3aa8bd339b140d1
akuma <diogerafael@gmail.com>
2020-03-25 22:35:41 -0300

    Removendo modulos desnecessários

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MainHeaderComponent} from './shared/main-header/main-header.component';
 import {LayoutModule} from '@angular/cdk/layout';
-import {AccountModule} from './pages/account/account.module';
-import {CategoryModule} from './pages/category/category.module';
 import {HttpService} from './shared/services/http-service';
 import {HttpClientModule} from '@angular/common/http';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {TransactionModule} from './pages/transaction/transaction.module';
 import {AngularMaterialModule} from './angular-material.module';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {SnackBarService} from './shared/services/snack-bar.service';
@@ -25,7 +21,6 @@ import {CurrencyMaskModule} from 'ng2-currency-mask';
 
 
 export function getBaseHref(platformLocation: PlatformLocation): string {
-  console.debug(platformLocation.getBaseHrefFromDOM());
   return platformLocation.getBaseHrefFromDOM();
 }
 
